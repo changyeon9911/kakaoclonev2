73f5a5228d4075bb1e8c05d9772bc3f17b2e0077
changyeon9911 <changyeon9911@naver.com>
2021-06-20 15:08:06 +0900

    s

--- upload_reminder.js
+++ upload_reminder.js
@@ -1,9 +1,8 @@
-const button = document.querySelector('input.chat__write')
+const button = document.querySelector('input.chat__write');
 
 function ClickButton(){
-    var inputfile = button.
     button.type = 'text';
     button.placeholder = "리마인딩 하고싶은 질문을 작성해주세요";
-}
+};
 
 button.addEventListener("change", ClickButton);
